e14b536741708e260e51576af65a0690ea9910bb
GLOCKDDD <1502339206@qq.com>
2025-12-21 23:00:48 +0800

    添加锁定系统

---
+++ Plugins/TheTarge56ea29da3a47V4/Intermediate/Build/Win64/UnrealEditor/Inc/TheTargetingSystem/UHT/TS_PlayerController.generated.h
@@ -0,0 +1,67 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "TS_PlayerController.h"
+
+#ifdef THETARGETINGSYSTEM_TS_PlayerController_generated_h
+#error "TS_PlayerController.generated.h already included, missing '#pragma once' in TS_PlayerController.h"
+#endif
+#define THETARGETINGSYSTEM_TS_PlayerController_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class ATS_PlayerController *****************************************************
+#define FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	DECLARE_FUNCTION(execTargetRight); \
+	DECLARE_FUNCTION(execTargetLeft); \
+	DECLARE_FUNCTION(execTargetCenter);
+
+
+THETARGETINGSYSTEM_API UClass* Z_Construct_UClass_ATS_PlayerController_NoRegister();
+
+#define FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATS_PlayerController(); \
+	friend struct Z_Construct_UClass_ATS_PlayerController_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend THETARGETINGSYSTEM_API UClass* Z_Construct_UClass_ATS_PlayerController_NoRegister(); \
+public: \
+	DECLARE_CLASS2(ATS_PlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TheTargetingSystem"), Z_Construct_UClass_ATS_PlayerController_NoRegister) \
+	DECLARE_SERIALIZER(ATS_PlayerController)
+
+
+#define FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h_15_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	ATS_PlayerController(ATS_PlayerController&&) = delete; \
+	ATS_PlayerController(const ATS_PlayerController&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATS_PlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATS_PlayerController); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATS_PlayerController) \
+	NO_API virtual ~ATS_PlayerController();
+
+
+#define FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h_12_PROLOG
+#define FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h_15_INCLASS_NO_PURE_DECLS \
+	FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class ATS_PlayerController;
+
+// ********** End Class ATS_PlayerController *******************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_build_U5M_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_TheTargetingSystem_Source_TheTargetingSystem_Public_TS_PlayerController_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
